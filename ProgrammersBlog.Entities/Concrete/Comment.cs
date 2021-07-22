b33d627b1bed72be358a1bb13e3360889fb8605d
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-07-22 17:24:23 +0300

    Errors in the comment and user classes have been removed

--- ProgrammersBlog.Entities/Concrete/Comment.cs
+++ ProgrammersBlog.Entities/Concrete/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Concrete
 {
-    public class Comment
+    public class Comment: EntityBase, IEntity
     {
         public string Text { get; set; }
         public int ArticleId { get; set; }
--- ProgrammersBlog.Entities/Concrete/User.cs
+++ ProgrammersBlog.Entities/Concrete/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Concrete
 {
-    public class User
+    public class User: EntityBase, IEntity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
