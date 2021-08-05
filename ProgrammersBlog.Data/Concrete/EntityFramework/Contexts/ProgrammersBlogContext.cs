3a934e05d399782967e13380b97ec9cf5c927047
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-08-05 17:00:36 +0300

    Map classes have been overrided with OnModelCreating method.

--- ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Mappings;
 using ProgrammersBlog.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -18,7 +19,16 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionString: @"");
+            optionsBuilder.UseSqlServer(connectionString: @"Server = (localdb)\ProjectsV13; Database = ProgrammersBlog; Trusted_Connection = True; Connect Timeout = 30; MultipleActiveResultSets = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new CommentMap());
+            modelBuilder.ApplyConfiguration(new RoleMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
         }
 
 
